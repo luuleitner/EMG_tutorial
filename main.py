1a681da32d56e3ed97383c0215626863795c6771
Christoph Leitner <chu.leitner@gmail.com>
2022-01-06 23:16:14 +0100

    added mnf and power spectrum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from scipy import signal
 from matplotlib import pyplot as plt
 from emg_processing.filter import Butter
 
@@ -17,23 +18,42 @@ ARG_BPF_HIGH = 320
 
 EMG_data_filtered = Butter(EMG_data, ARG_BPF_LOW, ARG_BPF_HIGH, ARG_SAMPLING_RATE, order=ARG_BPF_N)
 
-fig = plt.figure(figsize=(6,9), dpi = 300)
-ax_1 = fig.add_subplot(211)
-ax_1.plot(EMG_data,
+# fig = plt.figure(figsize=(6,9), dpi = 300)
+# ax_1 = fig.add_subplot(211)
+# ax_1.plot(EMG_data,
+#           markersize = 1,
+#           color='blue',
+#           label = 'x')
+# ax_1.set_ylabel('EMG raw [uV]')
+# ax_1.grid()
+#
+# ax_2 = fig.add_subplot(212)
+# ax_2.plot(EMG_data_filtered.data_filtered,
+#           markersize = 1,
+#           color='red',
+#           label = 'x')
+# ax_2.set_ylabel('EMG filtered [uV]')
+# ax_2.set_xlabel('Samples')
+# ax_2.grid()
+#
+# plt.subplots_adjust(hspace=0.2)
+# plt.show()
+
+
+## Welch's Power Spectrum
+f, Pxx_den = signal.welch(EMG_data_filtered.data_filtered, ARG_SAMPLING_RATE, nperseg=1024)
+fig1 = plt.figure(figsize=(6,3), dpi = 300)
+ax_1 = fig1.add_subplot(111)
+ax_1.plot(f, Pxx_den,
           markersize = 1,
           color='blue',
           label = 'x')
-ax_1.set_ylabel('EMG raw [uV]')
-ax_1.grid()
-
-ax_2 = fig.add_subplot(212)
-ax_2.plot(EMG_data_filtered.data_filtered,
-          markersize = 1,
-          color='red',
-          label = 'x')
-ax_2.set_ylabel('EMG filtered [uV]')
-ax_2.set_xlabel('Samples')
-ax_2.grid()
-
-plt.subplots_adjust(hspace=0.2)
+ax_1.set_xlabel('Frequency [Hz]')
+ax_1.set_ylabel('Power [W/Hz]')
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+ax_1.yaxis.major.formatter._useMathText = True
 plt.show()
+
+P_AOC = np.sum(Pxx_den)
+P_norm = Pxx_den / sg
+mnf = np.sum(f.T * gn)
